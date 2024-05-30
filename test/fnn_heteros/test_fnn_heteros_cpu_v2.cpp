c41ad7c466bc9f69d47c365ef0ea75f0983e081c
Miquel Florensa <91897595+miquelflorensa@users.noreply.github.com>
2024-05-30 17:51:46 -0400

    Update test_fnn_heteros_cpu_v2.cpp

--- test/fnn_heteros/test_fnn_heteros_cpu_v2.cpp
+++ test/fnn_heteros/test_fnn_heteros_cpu_v2.cpp
@@ -3,7 +3,7 @@
 // Description:  ...
 // Authors:      Luong-Ha Nguyen & James-A. Goulet
 // Created:      May 27, 2024
-// Updated:      May 27, 2024
+// Updated:      May 30, 2024
 // Contact:      luongha.nguyen@gmail.com & james.goulet@polymtl.ca
 // License:      This code is released under the MIT License.
 ////////////////////////////////////////////////////////////////////////////////
@@ -18,11 +18,16 @@ void fnn_heteros_old() {
         y_test_path;
     SavePath path;
     path.curr_path = get_current_dir();
-    std::string data_path = path.curr_path + "/data/toy_example";
-    x_train_dir = data_path + "/x_train_noise.csv";
-    y_train_dir = data_path + "/y_train_noise.csv";
-    x_test_dir = data_path + "/x_test_noise.csv";
-    y_test_dir = data_path + "/y_test_noise.csv";
+    // std::string data_path = path.curr_path + "/data/toy_example";
+    // x_train_dir = data_path + "/x_train_noise.csv";
+    // y_train_dir = data_path + "/y_train_noise.csv";
+    // x_test_dir = data_path + "/x_test_noise.csv";
+    // y_test_dir = data_path + "/y_test_noise.csv";
+    std::string data_path = path.curr_path + "/data/UCI/Boston_housing";
+    x_train_dir = data_path + "/x_train.csv";
+    y_train_dir = data_path + "/y_train.csv";
+    x_test_dir = data_path + "/x_test.csv";
+    y_test_dir = data_path + "/y_test.csv";
     x_train_path.push_back(x_train_dir);
     y_train_path.push_back(y_train_dir);
     x_test_path.push_back(x_test_dir);
@@ -31,30 +36,28 @@ void fnn_heteros_old() {
     // Create TAGI network
     Network network;
     network.layers = {1, 1, 1};
-    network.nodes = {1, 150, 2};
+    network.nodes = {13, 50, 2};
     network.activations = {0, 4, 0};
-    network.batch_size = 20;
+    network.batch_size = 1;
     network.noise_type = "heteros";
     network.sigma_v = 0.0;
     network.noise_gain = 1;
     network.init_method = "He";
-    network.multithreading = true;
+    //network.multithreading = true;
 
     TagiNetworkCPU net(network);
 
-    int n_epochs = 50;
+    int n_epochs = 1;
 
-    int n_x = 1;
+    int n_x = 13;
     int n_y = 1;
     std::vector<float> mu_x, sigma_x, mu_y, sigma_y;
     auto train_db = get_dataloader(x_train_path, y_train_path, mu_x, sigma_x,
-                                   mu_y, sigma_y, 800, n_x, n_y, true);
+                                   mu_y, sigma_y, 455, n_x, n_y, true);
     auto test_db = get_dataloader(x_test_path, y_test_path, train_db.mu_x,
                                   train_db.sigma_x, train_db.mu_y,
-                                  train_db.sigma_y, 200, n_x, n_y, true);
+                                  train_db.sigma_y, 51, n_x, n_y, true);
 
-    unsigned seed = 0;
-    std::default_random_engine seed_e(seed);
 
     // Number of data points
     int n_iter = train_db.num_data / net.prop.batch_size;
@@ -77,12 +80,12 @@ void fnn_heteros_old() {
 
     for (int e = 0; e < n_epochs; e++) {
         std::vector<float> V_batch(net.prop.batch_size * net.prop.n_y,
-                                   pow(net.prop.sigma_v, 2));
+                                    pow(net.prop.sigma_v, 2));
 
         for (int i = 0; i < n_iter; i++) {
             // Load data
             get_batch_idx(data_idx, i * net.prop.batch_size,
-                          net.prop.batch_size, batch_idx);
+                            net.prop.batch_size, batch_idx);
             get_batch_data(train_db.x, batch_idx, net.prop.n_x, x_batch);
             get_batch_data(train_db.y, batch_idx, net.prop.n_y, y_batch);
 
@@ -96,6 +99,7 @@ void fnn_heteros_old() {
             net.param_feed_backward();
         }
     }
+
 
     //////////////////////////////////////////////////////////////////////
     // Testing
@@ -116,8 +120,8 @@ void fnn_heteros_old() {
         mt_idx = i * net.prop.batch_size * net.prop.n_y;
 
         // Load data
-        get_batch_idx(data_idx, i * net.prop.batch_size, net.prop.batch_size,
-                      batch_idx);
+        get_batch_idx(data_idx, i * net.prop.batch_size,
+                        net.prop.batch_size, batch_idx);
         get_batch_data(test_db.x, batch_idx, net.prop.n_x, x_batch);
         get_batch_data(test_db.y, batch_idx, net.prop.n_y, y_batch);
 
@@ -125,7 +129,8 @@ void fnn_heteros_old() {
         net.feed_forward(x_batch, Sx_batch, Sx_f_batch);
 
         // Get hidden states for output layers
-        output_hidden_states(net.state, net.prop, ma_batch_out, Sa_batch_out);
+        output_hidden_states(net.state, net.prop, ma_batch_out,
+                                Sa_batch_out);
 
         // Update the final hidden state vector for last layer
         update_vector(ma_out, ma_batch_out, mt_idx, net.prop.n_y);
@@ -142,8 +147,7 @@ void fnn_heteros_old() {
         sy_norm[k] = pow(Sa_out[k], 0.5);
     }
     denormalize_mean(ma_out, test_db.mu_y, test_db.sigma_y, net.prop.n_y, my);
-    denormalize_mean(test_db.y, test_db.mu_y, test_db.sigma_y, net.prop.n_y,
-                     y_test);
+    denormalize_mean(test_db.y, test_db.mu_y, test_db.sigma_y, net.prop.n_y, y_test);
     denormalize_std(sy_norm, test_db.mu_y, test_db.sigma_y, net.prop.n_y, sy);
 
     // Compute metrics
@@ -180,35 +184,39 @@ void fnn_heteros_v2()
         y_test_path;
     SavePath path;
     path.curr_path = get_current_dir();
-    std::string data_path = path.curr_path + "/data/toy_example";
-    x_train_dir = data_path + "/x_train_noise.csv";
-    y_train_dir = data_path + "/y_train_noise.csv";
-    x_test_dir = data_path + "/x_test_noise.csv";
-    y_test_dir = data_path + "/y_test_noise.csv";
+    std::string data_path = path.curr_path + "/data/UCI/Boston_housing";
+    x_train_dir = data_path + "/x_train.csv";
+    y_train_dir = data_path + "/y_train.csv";
+    x_test_dir = data_path + "/x_test.csv";
+    y_test_dir = data_path + "/y_test.csv";
     x_train_path.push_back(x_train_dir);
     y_train_path.push_back(y_train_dir);
     x_test_path.push_back(x_test_dir);
     y_test_path.push_back(y_test_dir);
 
-    int n_x = 1;
+    int n_x = 13;
     int n_y = 1;
     std::vector<float> mu_x, sigma_x, mu_y, sigma_y;
     auto train_db = get_dataloader(x_train_path, y_train_path, mu_x, sigma_x,
-                                   mu_y, sigma_y, 800, n_x, n_y, true);
+                                   mu_y, sigma_y, 455, n_x, n_y, true);
     auto test_db = get_dataloader(x_test_path, y_test_path, train_db.mu_x,
                                   train_db.sigma_x, train_db.mu_y,
-                                  train_db.sigma_y, 200, n_x, n_y, true);
+                                  train_db.sigma_y, 51, n_x, n_y, true);
 
-    Sequential model(Linear(1, 150), ReLU(), Linear(150, 2), AGVI());
-    model.set_threads(8);
+    Sequential model(
+        Linear(13, 50),
+        ReLU(),
+        Linear(50, 2),
+        AGVI()
+    );
+    //model.set_threads(8);
 
     //////////////////////////////////////////////////////////////////////
     // Training
     //////////////////////////////////////////////////////////////////////
-    unsigned seed = 0;
-    std::default_random_engine seed_e(seed);
-    int batch_size = 20;
-    int iters = test_db.num_data / batch_size;
+
+    int batch_size = 1;
+    int iters = train_db.num_data / batch_size;
     std::vector<float> x_batch(batch_size * n_x, 0.0f);
     std::vector<float> y_batch(batch_size * n_y, 0.0f);
     std::vector<int> batch_idx(batch_size);
@@ -216,7 +224,7 @@ void fnn_heteros_v2()
 
     NoiseOutputUpdater output_updater(model.device);
 
-    for (int e = 0; e < 50; e++) {
+    for (int e = 0; e < 1; e++) {
         // if (e > 0) {
         //     // Shuffle data
         //     std::shuffle(data_idx.begin(), data_idx.end(), seed_e);
@@ -224,8 +232,8 @@ void fnn_heteros_v2()
         for (int i = 0; i < iters; i++) {
             // Load data
             get_batch_idx(data_idx, i * batch_size, batch_size, batch_idx);
-            get_batch_data(test_db.x, batch_idx, n_x, x_batch);
-            get_batch_data(test_db.y, batch_idx, n_y, y_batch);
+            get_batch_data(train_db.x, batch_idx, n_x, x_batch);
+            get_batch_data(train_db.y, batch_idx, n_y, y_batch);
 
             // Forward pass
             model.forward(x_batch);
@@ -242,7 +250,7 @@ void fnn_heteros_v2()
     //////////////////////////////////////////////////////////////////////
     // Testing
     //////////////////////////////////////////////////////////////////////
-    int test_batch_size = 20;
+    int test_batch_size = 1;
     int test_iters = test_db.num_data / test_batch_size;
     std::vector<float> test_x_batch(test_batch_size * n_x, 0.0f);
     std::vector<float> test_y_batch(test_batch_size * n_y, 0.0f);
@@ -268,8 +276,9 @@ void fnn_heteros_v2()
 
         // Collect the output data
         for (int j = 0; j < n_y * 2 * test_batch_size; j += 2) {
-            mu_a_batch_out[j / 2] = model.output_z_buffer->mu_a[j];
-            var_a_batch_out[j / 2] = model.output_z_buffer->mu_a[j + 1];
+            mu_a_batch_out[j/2] = model.output_z_buffer->mu_a[j];
+            var_a_batch_out[j/2] = model.output_z_buffer->var_a[j] +
+                                 model.output_z_buffer->mu_a[j+1];
         }
         update_vector(mu_a_out, mu_a_batch_out, mt_idx, n_y);
         update_vector(var_a_out, var_a_batch_out, mt_idx, n_y);
@@ -285,19 +294,16 @@ void fnn_heteros_v2()
     for (int k = 0; k < test_db.y.size(); k++) {
         sy_norm[k] = pow(var_a_out[k], 0.5);
     }
+
     denormalize_mean(mu_a_out, test_db.mu_y, test_db.sigma_y, n_y, my);
     denormalize_mean(test_db.y, test_db.mu_y, test_db.sigma_y, n_y, y_test);
     denormalize_std(sy_norm, test_db.mu_y, test_db.sigma_y, n_y, sy);
+
 
     // Compute metrics
     auto mse = mean_squared_error(my, y_test);
     auto log_lik = avg_univar_log_lik(my, y_test, sy);
 
-    // for (int i = 0; i < 200; i++) {
-    //     std::cout << "i: " << i << " mu_a_out: " << my[i]
-    //               << " y_test: " << y_test[i] << " sy: " << sy[i] <<
-    //               std::endl;
-    // }
 
     // Display results
     std::cout << "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
